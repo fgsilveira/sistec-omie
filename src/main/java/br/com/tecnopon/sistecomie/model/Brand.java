9a06d5c9565302b2c70babd433bbd69635618cd2
Flavio Silveira <flavio@devashightech.net>
2020-06-19 13:04:23 -0300

    Revert "Initial commit - Work in progress"
    This reverts commit 7d7be0b9

--- src/main/java/br/com/tecnopon/sistecomie/model/Brand.java
+++
@@ -1,7 +0,0 @@
-package br.com.tecnopon.sistecomie.model;
-
-public class Brand {
-
-    private Long id;
-    private String name;
-}
--- src/main/java/br/com/tecnopon/sistecomie/model/Employee.java
+++
@@ -1,9 +0,0 @@
-package br.com.tecnopon.sistecomie.model;
-
-public class Employee {
-
-    private Long id;
-    private String name;
-    private String username;
-    private String password;
-}
